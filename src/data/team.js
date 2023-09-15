9c1a7aa0914f217864adc064bc8181d7a98b721f
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-09-15 17:06:27 +0100

    Update team.js

--- src/data/team.js
+++ src/data/team.js
@@ -24,13 +24,14 @@ const team = [
     linkedIn: "http://linkedin.com/in/akhmim-abdelilah",
     github: "http://github.com/BrightProgrammer7",
     discord: "https://discordapp.com/users/753281875941916762",
-  },{
-    name: "Jericho McClellan",
-    job: "CSO|COO at Various",
-    image: JerichoImg,
-    linkedIn: "http://linkedin.com/in/akhmim-abdelilah",
-    github: "http://github.com/BrightProgrammer7",
-    discord: "https://discordapp.com/users/753281875941916762",
   },
+  // {
+  //   name: "Jericho McClellan",
+  //   job: "CSO|COO at Various",
+  //   image: JerichoImg,
+  //   linkedIn: "http://linkedin.com/in/akhmim-abdelilah",
+  //   github: "http://github.com/BrightProgrammer7",
+  //   discord: "https://discordapp.com/users/753281875941916762",
+  // },
 ];
 export default team;
